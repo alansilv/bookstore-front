1acc9f5aeabca7c293e8f383596e6c24af68c9e8
alansilv <alansilv@gmail.com>
2021-05-08 15:54:00 -0700

    Leitura de todos os livros por categoria implementado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -27,9 +27,10 @@ import { CategoriaCreateComponent } from './components/views/categoria/categoria
 import { FormsModule } from "@angular/forms";
 import { CategoriaDeleteComponent } from './components/views/categoria/categoria-delete/categoria-delete.component';
 import { CategoriaUpdateComponent } from './components/views/categoria/categoria-update/categoria-update.component';
+import { LivroReadAllComponent } from './components/views/livro/livro-read-all/livro-read-all.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent, NavComponent, HomeComponent, CategoriaReadComponent, CategoriaCreateComponent, CategoriaDeleteComponent, CategoriaUpdateComponent],
+  declarations: [AppComponent, HeaderComponent, FooterComponent, NavComponent, HomeComponent, CategoriaReadComponent, CategoriaCreateComponent, CategoriaDeleteComponent, CategoriaUpdateComponent, LivroReadAllComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
